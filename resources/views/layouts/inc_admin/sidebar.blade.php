563c24db6786c6dd60158c68c291be85fbfd3a60
AnnaStevi <stevianawudandari464@gmail.com>
2023-10-22 13:25:53 +0700

    menganti tampilan user admin

--- resources/views/layouts/inc_admin/sidebar.blade.php
+++ resources/views/layouts/inc_admin/sidebar.blade.php
@@ -13,6 +13,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           
 <x-nav-item label="Dashboard" icon="fas fa-tachometer-alt" :link="route('dashboard')" />
+<x-nav-item label="User Admin" icon="fas fa-users" :link="route('admin.index')" />
 
         </ul>
       </nav>

84d9b47634e7f569751c1b310f8e6f1ffd0f179a
AnnaStevi <stevianawudandari464@gmail.com>
2023-10-24 07:45:27 +0700

    menambahkan tampilan home dan kamar

--- resources/views/layouts/inc_tamu/navbar.blade.php
+++ resources/views/layouts/inc_tamu/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-info shadow">
     <div class="container">
-        <a class="navbar-brand h1" href="index.html">
+        <a class="navbar-brand h1" href="{{ route('home') }}">
             <img src="images/logo.JPEG" width="30" height="30" class="d-inline-block align-top img-circle" alt="Logo">
             Hotel Jember
         </a>
@@ -9,18 +9,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="kamar.html">Kamar</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Fasilitas</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="about.html">About</a>
-            </li>
+           <x-nav-item-tamu label="Home" :link="route('home')" />
+           <x-nav-item-tamu label="Kamar" :link="route('kamar')" />
         </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::view('kamar','kamar')->name('kamar');
 
 Route::view('admin','dashboard')->name('dashboard');
 Route::view('admin/admin','admin.index') ->name('admin.index');
